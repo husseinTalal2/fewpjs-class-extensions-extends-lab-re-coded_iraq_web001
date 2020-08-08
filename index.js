a18d63033eb060e91451956e1b33fe4697f9cc76
Hussein Talal <hossientalal2@gmail.com>
2020-08-08 14:29:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ class Polygon {
     return this.sidesArr.length;
   }
   get perimeter() {
+    if(!Array.isArray(this.sidesArr)) return;
     let sum=0;
     for(let i=0; i<this.countSides() ; i++){
       sum+=this.sidesArr[i];
