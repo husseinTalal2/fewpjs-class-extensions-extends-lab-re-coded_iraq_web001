a691454e4f9fdb15bb7d777ce9d8d192ae7f5e94
Hussein Talal <hossientalal2@gmail.com>
2020-08-08 14:29:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+class Polygon {
+  constructor(sidesArr){
+    this.sidesArr = sidesArr
+  }
+  get countSides() {
+    return this.sidesArr.length;
+  }
+  get perimeter() {
+    let sum=0;
+    for(let i=0; i<this.countSides() ; i++){
+      sum+=this.sidesArr[i];
+    }
+    return sum;
+  }
+}
